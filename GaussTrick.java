525090c585faf4f2467be21687990a26a42b3819
Alexander Antonov <72743980+AntonovJR@users.noreply.github.com>
2020-10-18 10:47:42 +0300

    Add files via upload

---
+++ GaussTrick.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class GaussTrick {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+        for (int i = 0; i < numbers.size() ; i++) {
+            if(i==numbers.size()-1){
+                break;
+            }
+            numbers.set(i,numbers.get(i) + numbers.get(numbers.size() - 1));
+            numbers.remove(numbers.size()-1);
+
+        }
+        System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
+    }
+}
